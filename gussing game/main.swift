a4da21d1a7adf8a3c4ebee7874da4fb411329061
Americo Lopez <americolopez@Interapts-MacBook-Pro-83.local>
2018-01-24 13:38:44 -0500

    added project files

---
+++ gussing game/main.swift
@@ -0,0 +1,47 @@
+//
+//  main.swift
+//  gussing game
+//
+//  Created by Americo Lopez on 1/23/18.
+//  Copyright © 2018 Americo Lopez. All rights reserved.
+//
+
+import Foundation
+
+
+while true{
+
+var randomnumber = arc4random_uniform(100)
+
+print("guess a number 0-100" )
+    var a = readLine()!
+
+var b = Int(a)!
+
+    if b == randomnumber{
+        print("you got it correct, do you wanna play again y/n ")
+    var c = readLine()!
+        
+    }
+        
+    else if b != randomnumber{
+        print("ooooo try again")
+        
+        
+    }
+    
+    
+    //if correct ask if they want to play again
+
+// if incorrect tell them the correct answer and ask them to play again
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+]
